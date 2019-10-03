838d35c70e304f84140c591bf9d6bc41e4dcc0a5
IP777 <ivanov_piter@ukr.net>
2019-10-03 14:08:18 +0300

    js hw-1 edits

--- javascript/goit-js-hw-01/js/task-5.js
+++ javascript/goit-js-hw-01/js/task-5.js
@@ -4,24 +4,28 @@ export function task5(){
 
     const country = prompt('В какую страну вы хотитете доставку товара?');
 
-    switch(country.toLowerCase()){
-        case (countryArr[0].toLowerCase()):
-            console.log(`Доставка в ${countryArr[0]} будет стоить ${costArr [0]}кредитов`);
-            break;
-        case (countryArr[1].toLowerCase()):
-            console.log(`Доставка в ${countryArr[1]} будет стоить ${costArr [1]}кредитов`);
-            break;
-        case (countryArr[2].toLowerCase()):
-            console.log(`Доставка в ${countryArr[2]} будет стоить ${costArr [2]}кредитов`);
-            break;
-        case (countryArr[3].toLowerCase()):
-            console.log(`Доставка в ${countryArr[3]} будет стоить ${costArr [3]}кредитов`);
-            break;
-        case (countryArr[4].toLowerCase()):
-            console.log(`Доставка в ${countryArr[4]} будет стоить ${costArr [4]}кредитов`);
-            break;
-        default:
-            console.log (`В стране "${country}" доставка не доступна`);
-            break;
+    if(country){
+        switch(country.toLowerCase()){
+            case (countryArr[0].toLowerCase()):
+                console.log(`Доставка в ${countryArr[0]} будет стоить ${costArr [0]}кредитов`);
+                break;
+            case (countryArr[1].toLowerCase()):
+                console.log(`Доставка в ${countryArr[1]} будет стоить ${costArr [1]}кредитов`);
+                break;
+            case (countryArr[2].toLowerCase()):
+                console.log(`Доставка в ${countryArr[2]} будет стоить ${costArr [2]}кредитов`);
+                break;
+            case (countryArr[3].toLowerCase()):
+                console.log(`Доставка в ${countryArr[3]} будет стоить ${costArr [3]}кредитов`);
+                break;
+            case (countryArr[4].toLowerCase()):
+                console.log(`Доставка в ${countryArr[4]} будет стоить ${costArr [4]}кредитов`);
+                break;
+            default:
+                console.log (`В стране "${country}" доставка не доступна`);
+                break;
+        }
+    }else{
+        console.log(`Доставка отменена`);
     }
 }
