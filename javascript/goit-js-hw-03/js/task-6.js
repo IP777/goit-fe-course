cf19cc10c621100793f851f83dd12c3925ac4288
IP777 <ivanov_piter@ukr.net>
2019-10-16 17:25:25 +0300

    hw3 task-6

--- javascript/goit-js-hw-03/js/task-6.js
+++ javascript/goit-js-hw-03/js/task-6.js
@@ -1,26 +1,21 @@
 export function task6() {
-    "user strict";
-    let input;
-    const numbers = [];
-    let total = 0;
-    let choiсe;
-
-    while (true) {
-        input = prompt("Введите произвольное число:");
-        input = Number(input);
-        if (!Number.isNaN(input) && input != 0) {
-            numbers.push(Number.parseInt(input));
-        } else if (Number.isNaN(input)) {
-            choiсe = confirm(`Вы ввели неправильное число. Продолжить?`);
-            if (!choiсe) {
-                break;
+    function calculateTotalPrice(arr, productName) {
+        for (const i of arr) {
+            if (i.name === productName) {
+                return `${i.name} ${i.quantity}шт стоит: ${i.price *
+                    i.quantity}грн`;
             }
-        } else {
-            for (let i = 0; i < numbers.length; i++) {
-                total += numbers[i];
-            }
-            alert(`Общая сумма чисел равна = ${total}`);
-            break;
         }
     }
+
+    const products = [
+        { name: "Радар", price: 1300, quantity: 4 },
+        { name: "Сканер", price: 2700, quantity: 3 },
+        { name: "Дроид", price: 400, quantity: 7 },
+        { name: "Захват", price: 1200, quantity: 2 }
+    ];
+
+    console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+    console.log(calculateTotalPrice(products, "Дроид")); // 2800
 }
