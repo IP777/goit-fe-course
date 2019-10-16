a9dd182fe6596e43cc412fe35ce1e8cf324d127c
IP777 <ivanov_piter@ukr.net>
2019-10-16 18:13:12 +0300

    hw3 edits

--- javascript/goit-js-hw-03/js/task-5.js
+++ javascript/goit-js-hw-03/js/task-5.js
@@ -10,13 +10,13 @@ export function task5() {
     }
 
     const products = [
-        { name: 'Радар', price: 1300, quantity: 4 },
-        { name: 'Сканер', price: 2700, quantity: 3 },
-        { name: 'Дроид', price: 400, quantity: 7 },
-        { name: 'Захват', price: 1200, quantity: 2 }
+        { name: "Радар", price: 1300, quantity: 4 },
+        { name: "Сканер", price: 2700, quantity: 3 },
+        { name: "Дроид", price: 400, quantity: 7 },
+        { name: "Захват", price: 1200, quantity: 2 }
     ];
 
-    console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
-    console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
-    console.log(getAllPropValues(products, 'category')); // []
+    console.table(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+    console.table(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
+    console.table(getAllPropValues(products, "category")); // []
 }
