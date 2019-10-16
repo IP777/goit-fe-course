e1697a2f8931664493ed92b01e010dd5f8cd45ad
IP777 <ivanov_piter@ukr.net>
2019-10-16 18:57:06 +0300

    hw3 task 7 edits

--- javascript/goit-js-hw-03/js/task-7.js
+++ javascript/goit-js-hw-03/js/task-7.js
@@ -1,4 +1,8 @@
 export function task7() {
+    /*Напиши скрипт управления личным кабинетом интернет банка.
+    Есть объект account в котором необходимо реализовать методы для работы с 
+    балансом и историей транзакций.*/
+
     /*
      * Типов транзацкий всего два.
      * Можно положить либо снять деньги со счета.
@@ -23,7 +27,14 @@ export function task7() {
          * Метод отвечающий за добавление суммы к балансу
          * Создает объект транзакции и вызывает addTransaction
          */
-        deposit(amount) {},
+        deposit(amount) {
+            this.balance += amount;
+            this.addTransaction({
+                id: Math.floor(Math.random() * 100),
+                type: Transaction.DEPOSIT,
+                amount: this.balance
+            });
+        },
 
         /*
          * Метод отвечающий за снятие суммы с баланса.
@@ -32,18 +43,36 @@ export function task7() {
          * Если amount больше чем текущий баланс, выводи сообщение
          * о том, что снятие такой суммы не возможно, недостаточно средств.
          */
-        withdraw(amount) {},
+        withdraw(amount) {
+            if (this.balance > amount) {
+                this.balance -= amount;
+                this.addTransaction({
+                    id: Math.floor(Math.random() * 100),
+                    type: Transaction.DEPOSIT,
+                    amount: this.balance
+                });
+            } else {
+                console.log(
+                    `Снятие суммы ${amount} невозможно недостаточно средств!`
+                );
+            }
+        },
 
         /*
          * Метод добавляющий транзацию в свойство transactions
          * Принимает объект трназкции
          */
-        addTransaction(transaction) {},
+        addTransaction(transaction) {
+            this.transactions.push(transaction);
+            console.log(this.transactions);
+        },
 
         /*
          * Метод возвращает текущий баланс
          */
-        getBalance() {},
+        getBalance() {
+            return this.balance;
+        },
 
         /*
          * Метод ищет и возвращает объект транзации по id
@@ -56,4 +85,9 @@ export function task7() {
          */
         getTransactionTotal(type) {}
     };
+
+    account.deposit(200);
+    account.deposit(400);
+    account.withdraw(300);
+    console.log(account.getBalance());
 }
