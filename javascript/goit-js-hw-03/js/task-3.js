385825c522ff5c9d53e70c3af8fa5f858f82665b
IP777 <ivanov_piter@ukr.net>
2019-10-18 11:11:48 +0300

    task-3 edits

--- javascript/goit-js-hw-03/js/task-3.js
+++ javascript/goit-js-hw-03/js/task-3.js
@@ -1,10 +1,25 @@
 export function task3() {
+    //Вариант 1
     function findBestEmployee(employees) {
         const keys = Object.keys(employees);
         const values = Object.values(employees);
         const maxValue = Math.max(...values);
 
         return keys[values.indexOf(maxValue)];
+    }
+
+    //Вариант 2
+    function findBestEmployee(employees) {
+        employees = Object.entries(employees);
+        let max = employees[0][1];
+        let employeesName = employees[0][0];
+        for (let i = 0; i < employees.length; i++) {
+            if (employees[i][1] > max) {
+                max = employees[i][1];
+                employeesName = employees[i][0];
+            }
+        }
+        return employeesName;
     }
 
     console.log(
