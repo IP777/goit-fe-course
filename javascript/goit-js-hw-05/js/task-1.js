fad7d4e58014a14462d9a9046828ec222afe9cd8
IP777 <ivanov_piter@ukr.net>
2019-10-22 17:40:56 +0300

    initial commit hw4

--- javascript/goit-js-hw-05/js/task-1.js
+++ javascript/goit-js-hw-05/js/task-1.js
@@ -1,26 +1,3 @@
 export function task1() {
-    const Hero = function(name, xp) {
-        this.name = name;
-        this.xp = xp;
-    };
-
-    Hero.prototype.gainXp = function(amount) {
-        console.log(`${this.name} gained ${amount} experience points`);
-        this.xp += amount;
-    };
-
-    const Warrior = function(name, xp, weapon) {
-        Hero.call(this, name, xp);
-
-        this.weapon = weapon;
-    };
-
-    Warrior.prototype = Object.create(Hero.prototype);
-    Warrior.prototype.constructor = Warrior;
-
-    Warrior.prototype.attack = function() {
-        console.log(`${this.name} attacks with ${this.weapon}`);
-    };
-
-    const poly = new Warrior("Poly", 200, "sword");
+    console.log("Hi hw4");
 }
