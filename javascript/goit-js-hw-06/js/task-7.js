0dfb34181c1690e56b7c80ae1fa40afb77804df7
IP777 <ivanov_piter@ukr.net>
2019-11-05 18:16:57 +0200

    hw6 add task 1,2,3,4,5,6,7,8

--- javascript/goit-js-hw-06/js/task-7.js
+++ javascript/goit-js-hw-06/js/task-7.js
@@ -1,77 +1,99 @@
 export function task7() {
-    const Transaction = {
-        DEPOSIT: 'deposit',
-        WITHDRAW: 'withdraw'
-    };
-
-    const account = {
-        balance: 0,
-        transactions: [],
-        amountArr: [],
-
-        deposit(amount) {
-            this.balance += amount;
-            this.addTransaction({
-                id: this.transactions.length,
-                type: Transaction.DEPOSIT,
-                amount: this.balance
-            });
+    const users = [
+        {
+            id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+            name: "Moore Hensley",
+            email: "moorehensley@indexia.com",
+            eyeColor: "blue",
+            friends: ["Sharron Pace"],
+            isActive: false,
+            balance: 2811,
+            skills: ["ipsum", "lorem"],
+            gender: "male",
+            age: 37
         },
-
-        withdraw(amount) {
-            if (this.balance > amount) {
-                this.balance -= amount;
-                this.addTransaction({
-                    id: this.transactions.length,
-                    type: Transaction.WITHDRAW,
-                    amount: this.balance
-                });
-            } else {
-                console.log(
-                    `Снятие суммы ${amount} невозможно недостаточно средств!`
-                );
-            }
+        {
+            id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+            name: "Sharlene Bush",
+            email: "sharlenebush@tubesys.com",
+            eyeColor: "blue",
+            friends: ["Briana Decker", "Sharron Pace"],
+            isActive: true,
+            balance: 3821,
+            skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+            gender: "female",
+            age: 34
         },
-
-        addTransaction(transaction) {
-            this.transactions.push(transaction);
+        {
+            id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+            name: "Ross Vazquez",
+            email: "rossvazquez@xinware.com",
+            eyeColor: "green",
+            friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+            isActive: false,
+            balance: 3793,
+            skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+            gender: "male",
+            age: 24
         },
-
-        getBalance() {
-            return this.balance;
+        {
+            id: "249b6175-5c30-44c6-b154-f120923736f5",
+            name: "Elma Head",
+            email: "elmahead@omatom.com",
+            eyeColor: "green",
+            friends: ["Goldie Gentry", "Aisha Tran"],
+            isActive: true,
+            balance: 2278,
+            skills: ["adipisicing", "irure", "velit"],
+            gender: "female",
+            age: 21
         },
-
-        getTransactionDetails(id) {
-            for (const i of this.transactions) {
-                if (i.id === id) {
-                    return i;
-                }
-            }
+        {
+            id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+            name: "Carey Barr",
+            email: "careybarr@nurali.com",
+            eyeColor: "blue",
+            friends: ["Jordan Sampson", "Eddie Strong"],
+            isActive: true,
+            balance: 3951,
+            skills: ["ex", "culpa", "nostrud"],
+            gender: "male",
+            age: 27
         },
-
-        getTransactionTotal(type) {
-            for (const i of this.transactions) {
-                if (i.type === type) {
-                    this.amountArr.push(i.amount);
-                    //return i.amount;
-                }
-            }
-            return this.amountArr;
+        {
+            guid: "150b00fb-dd82-427d-9faf-2879ea87c695",
+            name: "Blackburn Dotson",
+            email: "blackburndotson@furnigeer.com",
+            eyeColor: "brown",
+            friends: ["Jacklyn Lucas", "Linda Chapman"],
+            isActive: false,
+            balance: 1498,
+            skills: ["non", "amet", "ipsum"],
+            gender: "male",
+            age: 38
+        },
+        {
+            id: "e1bf46ab-7168-491e-925e-f01e21394812",
+            name: "Sheree Anthony",
+            email: "shereeanthony@kog.com",
+            eyeColor: "brown",
+            friends: ["Goldie Gentry", "Briana Decker"],
+            isActive: true,
+            balance: 2764,
+            skills: ["lorem", "veniam", "culpa"],
+            gender: "female",
+            age: 39
         }
+    ];
+
+    //Получить общую сумму баланса (поле balance) всех пользователей.
+
+    const calculateTotalBalance = (users) => {
+        return users.reduce((acc, value) => {
+            console.log(value.balance);
+            return acc + value.balance;
+        }, 0);
     };
 
-    //Проверки
-    account.deposit(200);
-    account.deposit(400);
-    account.withdraw(300);
-    console.log(`Текущий баланс = ${account.getBalance()}`);
-    //Выборка по id
-    console.log(account.getTransactionDetails(2));
-    console.log(
-        `Выюорка транзакций по типу ${account.getTransactionTotal(
-            Transaction.DEPOSIT
-        )}`
-    );
-    //Ошибка менше средств нежели сумма на счету
-    account.withdraw(700);
+    console.log(calculateTotalBalance(users)); // 20916
 }
