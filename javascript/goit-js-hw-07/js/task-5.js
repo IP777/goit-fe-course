85aa71e2bae8b4bebc1477688995bf7c1a6f2050
IP777 <ivanov_piter@ukr.net>
2019-11-18 21:35:44 +0200

    hw-7 task-5

--- javascript/goit-js-hw-07/js/task-5.js
+++ javascript/goit-js-hw-07/js/task-5.js
@@ -1,8 +1,25 @@
-export function task5() {
-    //Получить пользоваля (не массив) по email (поле email, он уникальный).
-    const getUserWithEmail = (users, email) =>
-        users.find(user => user.email === email);
+let textInput = document.querySelector('#name-input');
 
-    console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
-    console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
+
+/*<input type="text"> oninput: <span id="result"></span>
+<script>
+  var input = document.body.children[0];
+
+  input.oninput = function() {
+    document.getElementById('result').innerHTML = input.value;
+  };
+</script>*/
+
+const handleAddText = () => document.querySelector('#name-output').innerHTML = textInput.value;
+
+
+textInput.addEventListener('keydown', handleAddText)
+
+/*
+try{
+
+}catch{
+
 }
+*/
+
