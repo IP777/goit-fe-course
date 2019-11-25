ae828bd6af075e78daf7ae522cd7d6700eff8d5f
IP777 <ivanov_piter@ukr.net>
2019-11-25 21:28:02 +0200

    hw8 edits

--- javascript/goit-js-hw-08/js/main.js
+++ javascript/goit-js-hw-08/js/main.js
@@ -1,30 +1,35 @@
 import imageArr from "./gallery-items.js";
 
-const imageList = document.querySelector(".js-gallery");
+const refs = {
+    imageList: document.querySelector(".js-gallery")
+};
 
-imageList.innerHTML = createImage(imageArr);
+const galleryAction = {
+    createImage(imgArr) {
+        return imgArr
+            .map(imageIter => this.createTableRowMarkup(imageIter))
+            .join("");
+    },
+    createTableRowMarkup({ preview, original, description }) {
+        const img = `
+                    <li class="gallery__item">
+                    <a
+                      class="gallery__link"
+                      href="${original}"
+                    >
+                      <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                      />
+                    </a>
+                  </li> 
+                  `;
+
+        return img;
+    }
+};
+
+refs.imageList.innerHTML = galleryAction.createImage(imageArr);
 //imageList.insertAdjacentHTML("beforeend", imageStr);
-
-function createImage(imgArr) {
-    return imgArr.map(imageIter => createTableRowMarkup(imageIter)).join("");
-}
-
-function createTableRowMarkup({ preview, original, description }) {
-    const img = `
-                  <li class="gallery__item">
-                  <a
-                    class="gallery__link"
-                    href="${original}"
-                  >
-                    <img
-                      class="gallery__image"
-                      src="${preview}"
-                      data-source="${original}"
-                      alt="${description}"
-                    />
-                  </a>
-                </li> 
-                `;
-
-    return img;
-}
