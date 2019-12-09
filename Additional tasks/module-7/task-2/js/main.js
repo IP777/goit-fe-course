7765f057ae1b1f3b691bf9ab6f5af855ceb9d14a
IP777 <ivanov_piter@ukr.net>
2019-12-09 21:34:38 +0200

    Additional tasks edits 4

--- Additional tasks/module-7/task-2/js/main.js
+++ Additional tasks/module-7/task-2/js/main.js
@@ -1,33 +1,43 @@
-const menu = document.querySelector("#menu");
+// const menu = document.querySelector("#menu");
 
-class Menu {
-    constructor(elem) {
-        //this._elem = elem;
-        elem.onclick = this.onClick.bind(this);
+// class Menu {
+//     constructor(elem) {
+//         elem.onclick = this.onClick.bind(this);
+//     }
+
+//     save() {
+//         alert("сохранено");
+//     }
+
+//     load() {
+//         alert("загружено");
+//     }
+
+//     search() {
+//         alert("найдено");
+//     }
+
+//     clear() {
+//         alert("почистили");
+//     }
+
+//     onClick(event) {
+//         let action = event.target.dataset.action;
+//         if (action) {
+//             this[action]();
+//         }
+//     }
+// }
+
+// new Menu(menu);
+const area = document.querySelector("#area");
+
+const heandlerClick = e => {
+    const target = e.target;
+
+    if (target.dataset.action === "on_off") {
+        area.hidden = !area.hidden;
     }
+};
 
-    save() {
-        alert("сохранено");
-    }
-
-    load() {
-        alert("загружено");
-    }
-
-    search() {
-        alert("найдено");
-    }
-
-    clear() {
-        alert("почистили");
-    }
-
-    onClick(event) {
-        let action = event.target.dataset.action;
-        if (action) {
-            this[action]();
-        }
-    }
-}
-
-new Menu(menu);
+document.addEventListener("click", heandlerClick);
