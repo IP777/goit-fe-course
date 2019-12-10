33fcfb6964a5246d31825e55baa887df64ad6d2e
IP777 <ivanov_piter@ukr.net>
2019-12-10 13:02:40 +0200

    Additional tasks changes-6

--- Additional tasks/module-7/task-1/js/main.js
+++ Additional tasks/module-7/task-1/js/main.js
@@ -1,10 +1,22 @@
 const pool = document.getElementById("pool");
 const el = document.getElementById("person");
 
-const elHendler = e => {
-    el.style.top = e.clientY + "px";
-    el.style.left = e.clientX + "px";
+const elHendlerOver = e => {
+	el.hidden = false;
+	e.target.onmousemove = elHendlerMove;
 };
 
-pool.addEventListener("click", elHendler);
-//console.log(pool, el);
+const elHendlerOut = e => {
+	el.hidden = true;
+	e.target.onmousemove = elHendlerMove;
+};
+
+const elHendlerMove = e => {
+	el.style.top = e.clientY + "px";
+	el.style.left = e.clientX + "px";
+	el.textContent = `Position X: ${e.clientX}
+                      Position Y:${e.clientY} `;
+};
+
+pool.addEventListener("mouseover", elHendlerOver);
+pool.addEventListener("mouseout", elHendlerOut);
