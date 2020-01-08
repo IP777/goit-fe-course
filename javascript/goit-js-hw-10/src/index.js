fd88650b1eea4deb5e54d1bb752fe3a4123954fb
IP777 <ivanov_piter@ukr.net>
2020-01-08 18:55:35 +0200

    hw-10 commit-2

--- javascript/goit-js-hw-10/src/index.js
+++ javascript/goit-js-hw-10/src/index.js
@@ -4,7 +4,10 @@ import dataItem from './data/menu.json';
 
 const refs = {
   postFeed: document.querySelector('.js-menu'),
+  chbox: document.querySelector('input[type=checkbox]'),
 };
+
+console.log(refs.chbox);
 
 buildPostFeed(dataItem);
 
