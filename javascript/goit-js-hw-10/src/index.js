b4f47d017f895071ac0b7b3c051669712ca4973a
IP777 <ivanov_piter@ukr.net>
2020-01-08 18:47:32 +0200

    hw - 10 edits

--- javascript/goit-js-hw-10/src/index.js
+++ javascript/goit-js-hw-10/src/index.js
@@ -1,1 +1,15 @@
 import './styles.css';
+import menuTepmlate from './template/menu.hbs';
+import dataItem from './data/menu.json';
+
+const refs = {
+  postFeed: document.querySelector('.js-menu'),
+};
+
+buildPostFeed(dataItem);
+
+function buildPostFeed(posts) {
+  const markup = posts.map(post => menuTepmlate(post)).join('');
+
+  refs.postFeed.insertAdjacentHTML('beforeend', markup);
+}
