55d79711e85bf0aef57a14dee57f2cabb162eb34
IP777 <ivanov_piter@ukr.net>
2020-01-23 18:49:40 +0200

    hw-11 initial commit

---
+++ javascript/goit-js-hw-11/index.js
@@ -0,0 +1,43 @@
+const colors = [
+	"#FFFFFF",
+	"#2196F3",
+	"#4CAF50",
+	"#FF9800",
+	"#009688",
+	"#795548"
+];
+
+const randomIntegerFromInterval = (min, max) => {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+	startBtn: document.querySelector("[data-action=start]"),
+	stopBtn: document.querySelector("[data-action=stop]"),
+	body: document.body,
+	title: document.querySelector("#js-title")
+};
+
+let timerId = null;
+let isActive = false;
+
+const handleStartRandomizer = () => {
+	if (isActive) {
+		return;
+	}
+	isActive = true;
+	refs.title.innerHTML = "Timer started";
+	timerId = setInterval(() => {
+		refs.body.style.backgroundColor =
+			colors[randomIntegerFromInterval(1, 5)];
+	}, 1000);
+};
+const handleStopRandomizer = () => {
+	isActive = false;
+	refs.body.style.backgroundColor = colors[0];
+	refs.title.innerHTML = "Timer stopped";
+	clearInterval(timerId);
+};
+
+refs.startBtn.addEventListener("click", handleStartRandomizer);
+refs.stopBtn.addEventListener("click", handleStopRandomizer);
