5405e6be21fc0c665145a05ad9d397e967416b5c
IP777 <ivanov_piter@ukr.net>
2020-01-23 19:03:57 +0200

    hw 11 edits 2

--- javascript/goit-js-hw-11/js/timer.js
+++ javascript/goit-js-hw-11/js/timer.js
@@ -1,1 +1,13 @@
-console.log("Hi Me");
+// new CountdownTimer({
+//     selector: '#timer-1',
+//     targetDate: new Date('Jul 17, 2019'),
+//   });
+const time = Date.now();
+const days = Math.floor(time / (1000 * 60 * 60 * 24));
+const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+console.table(
+	`time ${time}, Days ${days}, Hours ${hours}, Mins ${mins}, Second ${secs}`
+);
