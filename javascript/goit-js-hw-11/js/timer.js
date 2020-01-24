c9f22e3501461e1a184afa95a15b2529c8b6deb3
IP777 <ivanov_piter@ukr.net>
2020-01-24 17:36:04 +0200

    hw-11 all

--- javascript/goit-js-hw-11/js/timer.js
+++ javascript/goit-js-hw-11/js/timer.js
@@ -10,7 +10,7 @@ class CountdownTimer {
 		};
 	}
 
-	timeFunc(time, param) {
+	timeFunc(time) {
 		const days = Math.floor(time / (1000 * 60 * 60 * 24));
 		const hours = Math.floor(
 			(time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -18,26 +18,16 @@ class CountdownTimer {
 		const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
 		const secs = Math.floor((time % (1000 * 60)) / 1000);
 
-		switch (param) {
-			case "secs":
-				return secs;
-			case "mins":
-				return mins;
-			case "hours":
-				return hours;
-			case "days":
-				return days;
-		}
+		this.refs.dayTitle.innerHTML = days;
+		this.refs.hoursTitle.innerHTML = hours;
+		this.refs.minsTitle.innerHTML = mins;
+		this.refs.secsTitle.innerHTML = secs;
 	}
 
 	start() {
 		this.timerId = setInterval(() => {
 			const time = this.targetDate - Date.now();
-			//---------------------------------------------------------------
-			this.refs.dayTitle.innerHTML = this.timeFunc(time, "days");
-			this.refs.hoursTitle.innerHTML = this.timeFunc(time, "hours");
-			this.refs.minsTitle.innerHTML = this.timeFunc(time, "mins");
-			this.refs.secsTitle.innerHTML = this.timeFunc(time, "secs");
+			this.timeFunc(time);
 		}, 1000);
 	}
 }
