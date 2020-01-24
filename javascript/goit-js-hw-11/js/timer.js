42d3ac870ada1b6414b1bde69bac07d9d7dcdade
IP777 <ivanov_piter@ukr.net>
2020-01-24 11:58:21 +0200

    hw-11 timer

--- javascript/goit-js-hw-11/js/timer.js
+++ javascript/goit-js-hw-11/js/timer.js
@@ -1,64 +1,37 @@
 class CountdownTimer {
-    constructor({ selector, targetDate }) {
-        //this.selector = document.querySelector(selector);
-        this.selector = selector;
-        this.targetDate = targetDate;
-        //this.dayTitle = this.selector.querySelector("[data-value=days]");
-        // this.hoursHours = selector.querySelector("[data-value=hours]");
-        // this.minsTitle = selector.querySelector("[data-value=mins]");
-        // this.secsTitle = selector.querySelector("[data-value=secs]");
-    }
+	constructor({ selector, targetDate }) {
+		this.elem = document.querySelector(selector);
+		this.targetDate = targetDate;
+		this.refs = {
+			dayTitle: this.elem.querySelector("[data-value=days]"),
+			hoursTitle: this.elem.querySelector("[data-value=hours]"),
+			minsTitle: this.elem.querySelector("[data-value=mins]"),
+			secsTitle: this.elem.querySelector("[data-value=secs]")
+		};
+	}
 
-    elem() {
-        return document.querySelector(this.selector);
-    }
-
-    getFullInfo() {
-        console.log(
-            `
-		  Selector ${this.selector}
-		  Target Data ${this.targetDate}
-		  Date ${this.elem()}
-		`,
-            this.elem()
-        );
-    }
+	start() {
+		this.timerId = setInterval(() => {
+			const time = this.targetDate - Date.now();
+			const days = Math.floor(time / (1000 * 60 * 60 * 24));
+			const hours = Math.floor(
+				(time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+			);
+			const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+			const secs = Math.floor((time % (1000 * 60)) / 1000);
+			//---------------------------------------------------------------
+			this.refs.dayTitle.innerHTML = days;
+			this.refs.hoursTitle.innerHTML = hours;
+			this.refs.minsTitle.innerHTML = mins;
+			this.refs.secsTitle.innerHTML = secs;
+		}, 1000);
+	}
 }
 
 const contTimer = new CountdownTimer({
-    selector: "#timer-1",
-    targetDate: new Date("Jul 17, 2019")
+	selector: "#timer-1",
+	targetDate: new Date("March 3, 2020")
 });
 
-contTimer.getFullInfo();
+contTimer.start();
 //--------------------------------------------------------------------
-/*
-const refs = {
-    dayTitle: document.querySelector("[data-value=days]"),
-    hoursHours: document.querySelector("[data-value=hours]"),
-    minsTitle: document.querySelector("[data-value=mins]"),
-    secsTitle: document.querySelector("[data-value=secs]")
-};
-
-const timer = {
-    start() {
-        const startTime = Date.now();
-        this.timerId = setInterval(() => {
-            const time = Date.now();
-            const days = Math.floor(time / (1000 * 60 * 60 * 24));
-            const hours = Math.floor(
-                (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-            );
-            const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-            const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-            console.log(
-                `time ${time}, Days ${days}, Hours ${hours}, Mins ${mins}, Second ${secs}`
-            );
-        }, 1000);
-    }
-};
-
-//timer.start();
-
-*/
