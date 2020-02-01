a4aa28082c640f438f57e7fce712a7d847dbed28
IP777 <ivanov_piter@ukr.net>
2020-02-01 14:57:02 +0200

    hw-11 edits

--- javascript/goit-js-hw-11/js/promis_task-3.js
+++ javascript/goit-js-hw-11/js/promis_task-3.js
@@ -1,49 +1,49 @@
 const refs = {
-	taskBtn: document.querySelector("[data-action=task-3]")
+    taskBtn: document.querySelector("[data-action=task-3]")
 };
 //---------------------------------------------------------
 
 const randomIntegerFromInterval = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 const makeTransaction = transaction => {
-	const delay = randomIntegerFromInterval(200, 500);
+    const delay = randomIntegerFromInterval(200, 500);
 
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			const canProcess = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const canProcess = Math.random() > 0.3;
 
-			if (canProcess) {
-				return resolve(transaction.id, delay);
-			}
-			reject(transaction.id);
-		}, delay);
-	});
+            if (canProcess) {
+                resolve(transaction.id, delay);
+            }
+            reject(transaction.id);
+        }, delay);
+    });
 };
 
 const logSuccess = (id, time) => {
-	console.log(`Transaction ${id} processed in ${time}ms`);
+    console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
 const logError = id => {
-	console.warn(`Error processing transaction ${id}. Please try again later.`);
+    console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
 refs.taskBtn.addEventListener("click", () => {
-	makeTransaction({ id: 70, amount: 150 })
-		.then(logSuccess)
-		.catch(logError);
+    makeTransaction({ id: 70, amount: 150 })
+        .then(logSuccess)
+        .catch(logError);
 
-	makeTransaction({ id: 71, amount: 230 })
-		.then(logSuccess)
-		.catch(logError);
+    makeTransaction({ id: 71, amount: 230 })
+        .then(logSuccess)
+        .catch(logError);
 
-	makeTransaction({ id: 72, amount: 75 })
-		.then(logSuccess)
-		.catch(logError);
+    makeTransaction({ id: 72, amount: 75 })
+        .then(logSuccess)
+        .catch(logError);
 
-	makeTransaction({ id: 73, amount: 100 })
-		.then(logSuccess)
-		.catch(logError);
+    makeTransaction({ id: 73, amount: 100 })
+        .then(logSuccess)
+        .catch(logError);
 });
